c181db57348e7aad3d6990ef2cec8946d3c1a0ab
andrzejkukuryk <101364440+andrzejkukuryk@users.noreply.github.com>
2022-10-02 20:59:45 +0200

    add scale

--- src/theory.js
+++ src/theory.js
@@ -1,3 +1,5 @@
 export const diatonic = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 export const sharped = ['C#', 'D#', 'F#', 'G#', 'A#'];
 export const flated = ['Db', 'Eb', 'Gb', 'Ab', 'Bb'];
+
+const scale = [... diatonic];
